9696cc1e68e20012f059a56983fc4759cb27a6b7
ArthurDelannoyazerty <ah.delannoy@gmail.com>
2025-03-23 18:35:06 +0100

    Added imports in UMLconstructor

--- src/uml_constructor.py
+++ src/uml_constructor.py
@@ -14,12 +14,13 @@ class UMLConstructor:
         self.packages:dict[list]    = dict()        # {"package1": [class1, class2], "package2": [class1, class2]}
         self.implements:list[tuple] = list()        # [(class_path1, class_path2), ...]    
         self.extends:list[tuple]    = list()        # [(class_path1, class_path2), ...]    
+        self.imports:list           = list()        # [class_path1, class_path2, ...]
 
-
-    def add_javafile(self, javafile:JavaFile) -> Self:
+    def add_javafile(self, javafile:JavaFile, add_imports:bool=False) -> Self:
         self.add_package(javafile.package, javafile.class_name)
         self.add_extend(javafile)
         self.add_implements(javafile)
+        if add_imports: self.add_imports(javafile)
         return self
 
 
@@ -37,6 +38,9 @@ class UMLConstructor:
         for implement_class_path in javafile.implements_class_path:
             self.implements.append((javafile.class_path, implement_class_path))
 
+    def add_imports(self, javafile:JavaFile) -> None:
+        self.imports.extend(javafile.imports)
+
 
     def get_uml(self) -> str:
         uml = '@startuml\n\n'
@@ -47,13 +51,17 @@ class UMLConstructor:
                 uml += f'\n\tclass {class_name}'
             uml += '\n}\n\n'
         
+        uml += '\n\n\'EXTENDS'
         for extend_source, extend_destination in self.extends:
             uml += f'\n{extend_source}  {UMLConstructor.ClassRelation.EXTEND}  {extend_destination}'
 
-        uml += '\n'
-
+        uml += '\n\n\'IMPLEMENTS'
         for implement_source, implement_destination in self.implements:
             uml += f'\n{implement_source}  {UMLConstructor.ClassRelation.IMPLEMENT}  {implement_destination}'
+        
+        uml += '\n\n\'IMPORTS'
+        for imports in self.imports:
+            uml += f'\nclass {imports}'
 
         uml += '\n\n@enduml'
         return uml
@@ -72,7 +80,7 @@ if __name__=='__main__':
     print(javafile)
 
     uml_constructor = UMLConstructor()
-    uml_constructor.add_javafile(javafile)
+    uml_constructor.add_javafile(javafile, add_imports=True)
     uml = uml_constructor.get_uml()
     print(uml)
 
